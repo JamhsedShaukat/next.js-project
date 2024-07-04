28bb255e3e26f0d046b67d66f10eafc40830a597
JamshedShaukat <168329984+JamhsedShaukat@users.noreply.github.com>
2024-07-04 00:48:08 -0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,10 +6,18 @@ import Provider from "@components/Provider";
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
+  verification: {
+    google: "2sXL7FI8HMCaI6MhspjJPyDhW1HRAAC9nCVtkCWG0Kw",
+  },
 };
 
 const RootLayout = ({ children }) => (
   <html lang='en'>
+    <head>
+      <meta name="google-site-verification" content="2sXL7FI8HMCaI6MhspjJPyDhW1HRAAC9nCVtkCWG0Kw" />
+      <title>{metadata.title}</title>
+      <meta name="description" content={metadata.description} />
+    </head>
     <body>
       <Provider>
         <div className='main'>
